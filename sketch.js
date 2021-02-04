0d63d48e33ae9aa75e9269497a3f715808c3a832
Purva Shukla <purvashukla828@gmail.com>
2021-02-04 14:15:54 +0530

    Updated code

--- sketch.js
+++ sketch.js
@@ -1,71 +1,107 @@
-// Angry Birds
-// Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/138-angry-birds.html
-// https://youtu.be/TDQzoe9nslY
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
 
-let ground;
-const boxes = [];
-let bird;
-let world, engine;
-let mConstraint;
-let slingshot;
+var gameState = "onSling";
+var score = 0;
+//game sounds
+var birdSelectSound,birdFlySound,pigSnortSound;
 
-let dotImg;
-let boxImg;
-let bkgImg;
-
-function preload() {
-  dotImg = loadImage('images/dot.png');
-  boxImg = loadImage('images/equals.png');
-  bkgImg = loadImage('images/skyBackground.jpg');
+function preload(){
+    backgroundImg = loadImage("sprites/skyBackground.jpg");
+    birdFlySound=loadSound("sounds/bird_flying.mp3")
+    pigSnortSound=loadSound("sounds/pig_snort.mp3")
+    birdSelectSound=loadSound("sounds/bird_select.mp3")
 }
 
-function setup() {
-  const canvas = createCanvas(1300, 650);
-  engine = Engine.create();
-  world = engine.world;
-  ground = new Ground(width / 2, height - 10, width, 20);
-  for (let i = 0; i < 6; i++) {
-    boxes[i] = new Box(1000, 300 - i * 75, 84, 100);
-  }
-  bird = new Bird(150, 300, 50);
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  slingshot = new SlingShot(150, 200, bird.body);
 
-  const mouse = Mouse.create(canvas.elt);
-  const options = {
-    mouse: mouse
-  };
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
-  // A fix for HiDPI displays
-  mouse.pixelRatio = pixelDensity();
-  mConstraint = MouseConstraint.create(engine, options);
-  World.add(world, mConstraint);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+    
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
-function keyPressed() {
-  if (key == ' ') {
-    World.remove(world, bird.body);
-    bird = new Bird(150, 200, 50);
-    slingshot.attach(bird.body);
-  }
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
 }
 
-function mouseReleased() {
-  setTimeout(() => {
+function mouseDragged(){
+    if (gameState!=="launched"){
+        birdSelectSound.play()
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
     slingshot.fly();
-  }, 100);
+    birdFlySound.play()
+    gameState = "launched";
+   
 }
 
-function draw() {
-  background(bkgImg);
-  Matter.Engine.update(engine);
-  ground.show();
-  for (let box of boxes) {
-    box.show();
-  }
-  slingshot.show();
-  bird.show();
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+       birdSelectSound.play()
+    }
 }
